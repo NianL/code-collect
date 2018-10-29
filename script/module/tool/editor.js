23e263925faa7e48b1cfe9bbb82fcd8b66681df7
NianLiu <ln8903@outlook.com>
2018-10-29 17:02:38 +0800

    .

--- script/module/tool/editor.js
+++ script/module/tool/editor.js
@@ -4,6 +4,10 @@ Vue.component('l-tool-editor', {
         <div v-if="importObject.status">
             <h3>文本编辑器 wangEditor</h3>
             <div id="editor"></div>
+            <div style="padding-top:5px;">
+                <a class="button" @click="clear">清空内容</a>
+                <a class="button" @click="output">导出内容</a>
+            </div>
         </div>
     `,
     data() {
@@ -21,9 +25,26 @@ Vue.component('l-tool-editor', {
         init() {
             this.$nextTick(() => {
                 var E = window.wangEditor;
-                var editor = new E('#editor');
-                editor.create();
+                this.editor = new E('#editor');
+                this.editor.create();
             });
+        },
+        clear() {
+            this.editor.txt.html('');
+        },
+        output() {
+            var fileName = (Date.parse(new Date()) / 1000) + ".txt";
+            download(fileName, this.editor.txt.html());
+
+            function download(filename, text) {
+                var element = document.createElement('a');
+                element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+                element.setAttribute('download', filename);
+                element.style.display = 'none';
+                document.body.appendChild(element);
+                element.click();
+                document.body.removeChild(element);
+            }
         }
     }
 });
